cbda7e6a1c3da08851e6bce8415cd8e7d203b8f2
Chris Holdgraf <choldgraf@berkeley.edu>
2016-08-16 17:48:50 -0700

    adding TFR plots and updating viz functions

--- neurods/viz.py
+++ neurods/viz.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import mne
 
+
 def find_squarish_dimensions(n):
     '''Get row,column dimensions for n elememnts
 
@@ -39,6 +40,7 @@ def find_squarish_dimensions(n):
     x = x[GoodOption]
     y = y[GoodOption]
     return x, y
+
 
 # Mark's slicing function. Perhaps fancier than we need. 
 def slice_3d_matrix(volume, axis=2, figh=None, vmin=None, vmax=None, cmap=plt.cm.gray, nr=None, nc=None ):
@@ -86,9 +88,9 @@ def slice_3d_matrix(volume, axis=2, figh=None, vmin=None, vmax=None, cmap=plt.cm
     return fig
 
 
-
 def plot_activity_on_brain(x, y, act, im, smin=10, smax=100, vmin=None,
-                           vmax=None, ax=None, cmap=None, name=None):
+                           vmax=None, alpha=None, ax=None, cmap=None,
+                           name=None):
     """Plot activity as a scatterplot on a brain.
 
     Parameters
@@ -109,6 +111,8 @@ def plot_activity_on_brain(x, y, act, im, smin=10, smax=100, vmin=None,
         The minimum color value / size cutoff
     vmax : float | None
         The maximum color value / size cutoff
+    alpha : float | ndarray | None
+        The alpha value for each channel.
     ax : axis | None
         An axis object to plot to
     cmap : matplotlib colormap | None
@@ -126,12 +130,16 @@ def plot_activity_on_brain(x, y, act, im, smin=10, smax=100, vmin=None,
         _, ax = plt.subplots()
     if cmap is None:
         cmap = plt.cm.coolwarm
+    alpha = 1. if alpha is None else alpha
+    if isinstance(alpha, (int, float, np.int)):
+        alpha = np.repeat(alpha, act.shape[0])
     vmin = act.min() if vmin is None else vmin
     vmax = act.max() if vmax is None else vmax
 
     # Define colors + sizes
     act_norm = (act - vmin) / float(vmax - vmin)
     colors = cmap(act_norm)
+    colors[:, -1] = alpha
     sizes = np.clip(np.abs(act) / float(vmax), 0, 1)  # Normalize bw 0 and 1
     sizes = MinMaxScaler((smin, smax)).fit_transform(sizes[:, np.newaxis])
 
@@ -176,7 +184,31 @@ def xy_to_layout(xy, image, ch_names=None, flipy=True):
 
 def plot_tfr(times, freqs, tfr, ax=None, use_log=True,
              baseline=None, linear_y=True):
-    """Plot a time-frequency representation."""
+    """Plot a time-frequency representation.
+
+    Parameters
+    ----------
+    times : array, shape (n_times,)
+        The timepoints (in seconds) for the TFR.
+    freqs : array, shape (n_freqs,)
+        The frequencies for each frequency band of the TFR.
+    tfr : array, shape (n_freqs, n_times)
+        The TFR to plot.
+    ax : matplotlib axis | None
+        Plot the TFR on a pre-created axis. If None, one will be created.
+    use_log : bool
+        Whether to take the log of each value before plotting.
+    baseline : array, shape (2,) | None
+        The tmin/tmax of an (optional) baseline.
+    linear_y : bool
+        Whether to force y-values in the plot to be linear. Useful if
+        frequencies are log-spaced.
+
+    Returns
+    -------
+    ax : matplotlib axis
+        The axis with the TFR plot.
+    """
     if ax is None:
         f, ax = plt.subplots()
     if use_log is True:
@@ -188,9 +220,66 @@ def plot_tfr(times, freqs, tfr, ax=None, use_log=True,
         vmin, vmax = (None, None)
     if linear_y is True:
         freqs = np.arange(freqs.shape[0])
-    ax.pcolormesh(times, freqs, tfr, cmap=plt.cm.coolwarm, vmin=vmin, vmax=vmax)
+    ax.pcolormesh(times, freqs, tfr, cmap=plt.cm.coolwarm,
+                  vmin=vmin, vmax=vmax)
     ax.set_xlim([times.min(), times.max()])
     ax.set_ylim([freqs.min(), freqs.max()])
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Frequency (Hz)')
     return ax
+
+
+def plot_tfr_topo(atfr, im, lt, baseline=(None, 0), mode='zscore',
+                  vmin=-5, vmax=5, cmap=None, show=True):
+    """Plot a topographic layout of an `AverageTFR` object.
+
+    Parameters
+    ----------
+    atfr : instance of MNE `AverageTFR` object.
+        The TFR data to plot
+    im : ndarray of an image, shape (n_pix_h, n_pix,w, {3|4})
+        The image to plot behind the TFR images.
+    lt : instance of MNE `layout` object
+        The layout corresponding to x/y locations of each electrode.
+    baseline : None (default) or tuple of length 2
+        The time interval to apply baseline correction.
+        If None do not apply it. If baseline is (a, b)
+        the interval is between "a (s)" and "b (s)".
+        If a is None the beginning of the data is used
+        and if b is None then b is set to the end of the interval.
+        If baseline is equal ot (None, None) all the time
+        interval is used.
+    mode : None | 'ratio' | 'zscore' | 'mean' | 'percent' | 'logratio' | 'zlogratio' # noqa
+        Do baseline correction with ratio (power is divided by mean
+        power during baseline) or zscore (power is divided by standard
+        deviation of power during baseline after subtracting the mean,
+        power = [power - mean(power_baseline)] / std(power_baseline)), mean
+        simply subtracts the mean power, percent is the same as applying ratio
+        then mean, logratio is the same as mean but then rendered in log-scale,
+        zlogratio is the same as zscore but data is rendered in log-scale
+        first.
+        If None no baseline correction is applied.
+    vmin : float | None
+        The mininum value an the color scale. If vmin is None, the data
+        minimum value is used.
+    vmax : float | None
+        The maxinum value an the color scale. If vmax is None, the data
+        maximum value is used.
+    cmap : matplotlib colormap | str
+        The colormap to use. Defaults to 'RdBu_r'.
+    show : bool
+        Whether to show the figure after creating it.
+
+    Returns
+    -------
+    fig : matplotlib `Figure`
+        The figure with TFR plots embedded
+    """
+    cmap = plt.cm.coolwarm if cmap is None else cmap
+    fig = atfr.plot_topo(picks=range(len(atfr.ch_names)), layout=lt,
+                         baseline=(None, 0), mode='zscore', vmin=-5, vmax=5,
+                         cmap=plt.cm.coolwarm, show=False)
+    mne.viz.add_background_image(fig, im)
+    if show is True:
+        fig.show()
+    return fig

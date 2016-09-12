5c61570c36700f64a93d56401242e4d38d3837e2
Chris Holdgraf <choldgraf@berkeley.edu>
2016-09-12 11:47:21 -0700

    adding a function to create student labs wo answers

--- neurods/io.py
+++ neurods/io.py
@@ -4,9 +4,13 @@ import pandas as pd
 import shutil as sh
 import os.path as op
 import os
+import nbformat
+import sys
 from glob import glob
 from zipfile import ZipFile
 from mne.utils import _fetch_file
+from subprocess import check_output
+from importlib import reload
 
 
 path_data = '/home/shared/cogneuro-connector/data/'
@@ -104,3 +108,71 @@ def download_file(url, name, root_destination='~/data/', zipfile=False,
                              'overwrite'.format(out_path))
         _fetch_file(download_path, out_path)
     print('Successfully moved file to {}'.format(out_path))
+
+
+def strip_answers(nbpath, strip_string='### STUDENT ANSWER',
+                  output_suffix='student', save=True,
+                  clean_outputs=True, path_save=None):
+    """Clean inputs / outputs of notebooks to send to students.
+
+    Parameters
+    ----------
+    nbpath : str
+        The path to a jupyter notebook file.
+    strip_string : str
+        A string to search for in input cells. If the string is
+        found, then anything in the cell AFTER the string is removed.
+    output_suffix : str
+        An output to be appended to a saved jupyter notebook.
+    save : bool
+        Whether to save the output notebook.
+    clean_outputs : bool
+        Whether to clear outputs for the cells before saving.
+    path_save : string
+        An optional path to a folder where the output notebook will
+        be saved. If None, save in the same directory as nbpath.
+
+    Outputs
+    -------
+    nb : instance of NotebookNode
+        The NotebookNode corresponding to the cleaned notebook.
+    """
+    nb = nbformat.read(nbpath, 4)
+    for ii, cell in enumerate(nb['cells']):
+        # Only check code cells
+        if cell['cell_type'] != 'code':
+            continue
+
+        # Replace some input cells
+        ix = cell['source'].find(strip_string)
+        if ix != -1:
+            newstr = cell['source'][:ix + len(strip_string)] + '\n'
+            cell['source'] = newstr
+
+        # Clean outputs
+        if clean_outputs is True:
+            cell['outputs'] = []
+
+        # Clear prompt numbers
+        cell['execution_count'] = None
+        cell.pop('prompt_number', None)
+
+    if save is True:
+        filename = os.path.basename(nbpath)
+        path_save = os.path.dirname(nbpath) if path_save is None else path_save
+        name, ext = filename.split('.')
+        outname = '{}_{}.{}'.format(name, output_suffix, ext)
+        save_file = '{}{}{}'.format(path_save, os.sep, outname)
+        print('Saving to {}'.format(save_file))
+        if not os.path.isdir(path_save):
+            os.makedirs(path_save)
+        nbformat.write(nb, save_file)
+    return nb
+
+
+def update_neurods():
+    s = ('pip install git+https://github.com/choldgraf/neurods.git --user '
+         '--upgrade')
+    s = check_output(s.split(' '))
+    s = s.decode('utf-8')
+    print(s)
